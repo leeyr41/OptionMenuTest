217bb39be06f9b1bdfdb628a5b96654157719670
leeyr41 <leeyr41@naver.com>
2017-05-23 19:59:53 +0900

    #4 mingming

--- app/src/main/java/leeyr41/optionmenutest/MainActivity.java
+++ app/src/main/java/leeyr41/optionmenutest/MainActivity.java
@@ -1,17 +1,60 @@
 package leeyr41.optionmenutest;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout linear;
+    Button but;
+    private boolean ture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        linear=(LinearLayout)findViewById(R.id.linear);
+        but=(Button)findViewById(R.id.but_ok);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater mInflater=getMenuInflater();
+        mInflater.inflate(R.menu.menu1, menu);
+        return ture;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch(item.getItemId()){
+            case R.id.item_blue:
+                linear.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.item_red:
+                linear.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.item_yellow:
+                linear.setBackgroundColor(Color.YELLOW);
+                return true;
+            case R.id.item_but_rotate:
+                but.setRotation(but.getRotation()+45);
+                return true;
+            case R.id.item_but_size:
+                but.setScaleX(but.getTextScaleX()+2);
+                return true;
+
+        }
+        return ture;
+    }
 
 }
 
